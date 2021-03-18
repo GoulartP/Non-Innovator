caca94494d72061734193398213afd2ee61e3463
GoulartP <goulartphga@gmail.com>
2021-03-18 07:55:06 -0300

    Melhorando Pulo

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -18,9 +18,13 @@ public class ThirdPersonMovement : MonoBehaviour
     public float walk = 10f;
     public float sprintspeed = 20f;
 
+    public bool isSprinting = false;
+    public float sprintMultiplier;
+
     public float gravity = -9.81f;
-    public float turnSmoothTime = 0.1f;
     public float jumpHeight = 3f;
+
+    public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     Vector3 velocity;
@@ -46,60 +50,66 @@ public class ThirdPersonMovement : MonoBehaviour
         }
         else
         {
+            
             isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groudMask);
+
             if (isGrounded && velocity.y < 0)
             {
                 velocity.y = -2f;
             }
-
-
-            float horizontal = Input.GetAxisRaw("Horizontal"); // Raw tem resposta imediata
-            float vertical = Input.GetAxisRaw("Vertical"); // Raw tem resposta imediata
-
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+            
             velocity.y += gravity * Time.deltaTime;
-
-            controller.Move(velocity * Time.deltaTime);
 
             if (Input.GetButtonDown("Jump") && isGrounded)
             {
-                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity + speed);
+                velocity.y = Mathf.Sqrt(jumpHeight * -1 * gravity + speed);
             }
 
             if (Input.GetKey((KeyCode.LeftShift)) && isGrounded)
             {
+                isSprinting = true;
                 speed = sprintspeed;
 
                 if (Input.GetButtonDown("Jump"))
                 {
                     velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity + speed);
+
                 }
             }
 
             else
             {
+                isSprinting = false;
                 speed = walk;
             }
 
+            controller.Move(velocity * Time.deltaTime);
+
+            float horizontal = Input.GetAxisRaw("Horizontal"); // Raw tem resposta imediata
+            float vertical = Input.GetAxisRaw("Vertical"); // Raw tem resposta imediata
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+            
             if (direction.magnitude >= 0.1f)
 
             {
                 float targetAngle =
                     Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
                     cam.eulerAngles.y; //Pega Angulação entre os eixos
+
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
                     turnSmoothTime); //Dados para Rotação suave
 
                 transform.rotation = Quaternion.Euler(0f, angle, 0f); //Faz a rotação (diagonais)
-
+               
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
+                
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
-
+            
             if (Input.GetKey((KeyCode.R)))
                 Restart();
+
         }
 
     }
